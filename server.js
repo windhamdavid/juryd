ac5acd506ab8e5c51e97d3fcd833db3c91431bb3
windhamdavid <david@davidawindham.com>
2015-12-03 15:41:46 -0500

    wherever I was

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ var build = require('./build');
 var express = require('express');
 var app = express();
 
-var config = require('./env.json')[process.env.NODE_ENV || 'development'];
+//var config = require('./config.json')[process.env.NODE_ENV || 'development'];
 
 var env = process.env.NODE_ENV || 'development';
 if ('development' == env) {
-   
+   //Changed for Express 4
 }
+
+
+
 app.use(express.static(__dirname + '/build'));
 
+var port = process.env.PORT || 3000;
+
 app.listen(process.env.PORT, function(){
-  console.log('Server running at http://localhost:' + process.env.PORT + '');
+  console.log('running at http://localhost:' + process.env.PORT + '');
 });
 
